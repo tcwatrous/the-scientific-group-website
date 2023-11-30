3316e1ee347e16e3feeffc8130ac510e2c8d5ba8
Tim Watrous <tcwatrous@users.noreply.github.com>
2023-11-30 13:31:09 -0500

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -239,16 +239,45 @@
     animationOutSpeed: 1000, // Animation out speed (ms)
     scrollText: '<span class="lnr lnr-chevron-up"></span>' // Text for element
   });
-  
+
   if($("#search-input").length >0){
-    
+
     var sjs = SimpleJekyllSearch({
       searchInput: document.getElementById('search-input'),
       resultsContainer: document.getElementById('results-container'),
       json: '/search.json'
     });
   }
+  var form = document.getElementById("contact-form");
 
+   async function handleSubmit(event) {
+     event.preventDefault();
+     var status = document.getElementById("my-form-status");
+     var data = new FormData(event.target);
+     fetch(event.target.action, {
+       method: form.method,
+       body: data,
+       headers: {
+           'Accept': 'application/json'
+       }
+     }).then(response => {
+       if (response.ok) {
+         status.innerHTML = "Thanks for your submission!";
+         form.reset()
+       } else {
+         response.json().then(data => {
+           if (Object.hasOwn(data, 'errors')) {
+             status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+           } else {
+             status.innerHTML = "Oops! There was a problem submitting your form"
+           }
+         })
+       }
+     }).catch(error => {
+       status.innerHTML = "Oops! There was a problem submitting your form"
+     });
+   }
+   form.addEventListener("submit", handleSubmit)
 
 
 })(jQuery);
